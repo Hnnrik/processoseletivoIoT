97d792d5bceb249dd31620ab980f9805fe65e639
Hnnrik <pedrohenriquebzzr@gmail.com>
2026-04-20 13:36:59 +0000

    testando o limite de tempo da simulação

--- src/main.py
+++ src/main.py
@@ -35,7 +35,7 @@ while True:
 
     tempo_atual = time.ticks_ms()
     tempo_passado = time.ticks_diff(tempo_atual, inicio)
-    if tempo_passado >= 9000:
+    if tempo_passado >= 1000:
         break
 
     try:
