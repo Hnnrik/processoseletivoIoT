c96a9168437f53c85871775fe61760fa550579ba
Hnnrik <pedrohenriquebzzr@gmail.com>
2026-04-20 13:25:54 +0000

    Diminuindo o tempo da simulação

--- src/main.py
+++ src/main.py
@@ -35,7 +35,7 @@ while True:
 
     tempo_atual = time.ticks_ms()
     tempo_passado = time.ticks_diff(tempo_atual, inicio)
-    if tempo_passado >= 9300:
+    if tempo_passado >= 9000:
         break
 
     try:
@@ -111,7 +111,7 @@ while True:
 
         oled.show()
 
-        time.sleep(1)
+        time.sleep(0.4)
 
     except Exception as e:
 
